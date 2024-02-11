8c17e2760cc98b127e3785ed150f73d1502fcab6
André Martins <andre.lucas.medeir@gmail.com>
2024-02-10 21:57:54 -0300

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,14 +24,15 @@ export default function RootLayout({
         <meta name="description" content="Lista de Presente" />
 
         <meta property="twitter:title" content="Vitória & André" />
-        <meta property="twitter:image" content="/thumbnail" />
+        <meta property="twitter:image" content="/thumbnail.png" />
         <meta property="twitter:image:type" content="image/png" />
         <meta property="twitter:description" content="Lista de Presentes" />
         <meta property="twitter:image:width" content="1200" />
         <meta property="twitter:image:height" content="630" />
 
         <meta property="og:title" content="Vitória & André" />
-        <meta property="og:image" content="/thumbnail" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/thumbnail.png" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:description" content="Lista de Presentes" />
         <meta property="og:image:width" content="1200" />
