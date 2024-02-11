4cb06b62453cbb5d2c51a11f8e88c5b14c10af2c
André Martins <andre.lucas.medeir@gmail.com>
2024-02-11 15:44:27 -0300

    fixes

--- src/app/sections/Separator.tsx
+++ src/app/sections/Separator.tsx
@@ -2,7 +2,7 @@ export default function Separator() {
   return (
     <section className="grid laptop:grid-cols-2 h-0 micro:h-[50px] laptop:h-[100px] container">
       <div></div>
-      <div className="bg-wine rounded-b-full w-full h-[150px] mobile:h-[300px] tablet:max-w-[560px] mx-auto laptop:h-[400px]"></div>
+      <div className="bg-wine rounded-b-full w-full h-[150px] micro:h-[200px] mobile:h-[300px] tablet:max-w-[560px] mx-auto laptop:h-[400px]"></div>
     </section>
   );
 }
