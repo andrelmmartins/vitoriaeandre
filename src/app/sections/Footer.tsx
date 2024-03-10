a4f3ac073122eb266ecc3698bf32a57e2bb9b8f8
André Martins <andre.lucas.medeir@gmail.com>
2024-03-10 19:44:17 -0300

    style: fotter have a inside container

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="container bg-wine mt-[40px] py-[48px]">
-      <p className="text-md text-beige">
-        Desenvolvido por André{" "}
-        <span className="text-sm text-wine-light">
-          (Gerenciado por Vitória)
-        </span>
-      </p>
+    <footer className="bg-wine mt-[40px] py-[48px]">
+      <div className="container">
+        <p className="text-md text-beige">
+          Desenvolvido por André{" "}
+          <span className="text-sm text-green">(Gerenciado por Vitória)</span>
+        </p>
+      </div>
     </footer>
   );
 }
